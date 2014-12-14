cca36652a87f6fab80f535604822ea384ee9182d
Greg Oppman <Greg.Oppman@WUSJLLPK0V29X.AM.JLLNET.com>
2014-12-14 15:54:24 -0500

    Animate squares diagnally

--- js/pendulumn.js
+++ js/pendulumn.js
@@ -1,3 +1,4 @@
+
 function canvas() {
   var context = myCanvas.getContext('2d');
   var x = 0, y=0, width = myCanvas.width, height = myCanvas.height;
@@ -13,19 +14,21 @@ function canvas() {
       var n = 10;
       width = 20;
       height = 20;
-      while (i < n) {
-        x = i/10 * myCanvas.width;
+      var time = 0;
+      var redraw = function (){
+        context.clearRect(0,0, myCanvas.width, myCanvas.height);
+      for (i=0; i < n ;i++) {
+        x = i/10 * myCanvas.width + time;
         y = i/10 * myCanvas.height;
         context.fillRect(x,y, width, height);
         i++;
-
-
-
-      }
-
-
+        }
+      time ++;
+    }
+    var interval = setInterval( redraw , 20 );
     }
     drawManyRectangles()
+
 }
 
 canvas();
