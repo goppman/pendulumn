c739a574133c120030d5fdfdc5da3c781b4a6e66
Greg Oppman <Greg.Oppman@WUSJLLPK0V29X.AM.JLLNET.com>
2014-12-14 16:06:53 -0500

    pedulumn Move Anim

--- js/pendulumn.js
+++ js/pendulumn.js
@@ -2,7 +2,11 @@
 function canvas() {
   var context = myCanvas.getContext('2d');
   var x = 0, y=0, width = myCanvas.width, height = myCanvas.height;
+  var interval;
 
+  function stop() {
+    clearInterval(interval);
+  }
     function drawRectangle() {
     context.fillStyle = '#bc141a';
     context.fillRect(0,0,100,140);
@@ -10,20 +14,20 @@ function canvas() {
     }
 
     function drawManyRectangles() {
-      var i = 0;
-      var n = 10;
+      //var i = 10;
+      var n = 20;
       width = 20;
-      height = 20;
+      height = 5;
       var time = 0;
       var redraw = function (){
         context.clearRect(0,0, myCanvas.width, myCanvas.height);
       for (i=0; i < n ;i++) {
-        x = i/10 * myCanvas.width + time;
-        y = i/10 * myCanvas.height;
+        x = myCanvas.width / 2 + Math.sin(time *i) *05;
+        y = i/n * myCanvas.height + Math.cos(time *i) *05;
         context.fillRect(x,y, width, height);
         i++;
         }
-      time ++;
+      time +=.01;
     }
     var interval = setInterval( redraw , 20 );
     }
